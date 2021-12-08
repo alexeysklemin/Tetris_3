14bfd3d3e96b581a5568a6533692e1558643efb3
нет2 каби <a_sklemin@mail.ru>
2021-12-08 21:04:12 +0400

    Structure of project by Vladislav _Fano_

---
+++ SFML_Project/Canvas.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <Windows>
+using namespace sf;
+
+class Canvas{
+public:
+    canvas();
+};
+#pragma once
+
--- SFML_Project/Tetris.cpp
+++ SFML_Project/Tetris.cpp
@@ -1,23 +1,78 @@
 #include <SFML/Graphics.hpp>
 
+
+
+
+
+bool check()
+{
+    for (int i = 0; i < 4; i++)
+        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M) return 0;
+        else if (field[a[i].y][a[i].x]) return 0;
+
+    return 1;
+};
+
+class tetris() {
+private:
+    gameField(m_gameField);
+};
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    srand(time(0));
+
+    RenderWindow window(VideoMode(320, 480), "The Game!");
+
+    Texture t1, t2, t3;
+    t1.loadFromFile("images/tiles.png");
+    t2.loadFromFile("images/background.png");
+    t3.loadFromFile("images/frame.png");
+
+    Sprite s(t1), background(t2), frame(t3);
+
+    int dx = 0; bool rotate = 0; int colorNum = 1;
+    float timer = 0, delay = 0.3;
+
+    Clock clock;
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
+        Event e;
+        while (window.pollEvent(e))
         {
-            if (event.type == sf::Event::Closed)
+            if (e.type == Event::Closed)
                 window.close();
+
+            if (e.type == Event::KeyPressed)
+                if (e.key.code == Keyboard::Up) rotate = true;
+                else if (e.key.code == Keyboard::Left) dx = -1;
+                else if (e.key.code == Keyboard::Right) dx = 1;
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
+
+        //// <- Move -> ///
+        move();
+
+        //////Rotate//////
+        if (rotate)
+            rotate();
+
+        ///////Tick//////
+        if (timer > delay)
+            tick();
+
+        ///////check lines//////////
+        checklines();
+
+        /////////draw//////////
+        paint();
     }
 
     return 0;
